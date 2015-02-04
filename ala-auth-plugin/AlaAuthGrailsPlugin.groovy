b46de46cfa3a5b0c24f18c1d2cee37d787cdccd2
jamirali <jamirali@hotmail.com>
2015-02-05 09:55:40 +1100

    fix cas server config reference

--- ala-auth-plugin/AlaAuthGrailsPlugin.groovy
+++ ala-auth-plugin/AlaAuthGrailsPlugin.groovy
@@ -174,7 +174,7 @@ class AlaAuthGrailsPlugin {
         def config = application.config
 
         if (!config.userDetails.url) {
-            config.userDetails.url = Holders.config.security.casServerName + "/userdetails/userDetails/"
+            config.userDetails.url = Holders.config.security.cas.casServerName + "/userdetails/userDetails/"
         }
 
         if (!config.userDetails.path) {
