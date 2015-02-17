b627e3a278b0a748bcf4ccd6c7b47a0e012e063f
Angel Ruiz <aruizca@gmail.com>
2015-02-18 10:13:14 +1100

    Prepare next development version 1.1-SNAPSHOT

--- ala-auth-plugin/AlaAuthGrailsPlugin.groovy
+++ ala-auth-plugin/AlaAuthGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import grails.util.Holders
 
 class AlaAuthGrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "1.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // the other plugins this plugin depends on
