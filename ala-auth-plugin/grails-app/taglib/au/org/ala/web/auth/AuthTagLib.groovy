88566348747d53399a9fd5c0c24b0df857739685
nickdos <nickdos@gmail.com>
2015-02-23 16:12:26 +1100

    Added loginLout tag lib method
    So 3rd party skinned apps don't need ala-bootstrap for this one
    function. Method copied from ala-web-theme (ala-bootstrap).

--- ala-auth-plugin/grails-app/taglib/au/org/ala/web/auth/AuthTagLib.groovy
+++ ala-auth-plugin/grails-app/taglib/au/org/ala/web/auth/AuthTagLib.groovy
@@ -1,9 +1,17 @@
 package au.org.ala.web.auth
 
+import au.org.ala.cas.util.AuthenticationCookieUtils
+import grails.util.Holders
+
 class AuthTagLib {
 
     def authService
     def securityPrimitives // gets injected in AlaWebThemeGrailsPlugin 'doWithSpring' section
+
+    def grailServerURL = Holders.config.grails.serverURL ?: "http://bie.ala.org.au"
+    // the next two can also be overridden by tag attributes
+    def casLoginUrl = Holders.config.security.cas.loginUrl ?: "https://auth.ala.org.au/cas/login"
+    def casLogoutUrl = Holders.config.security.cas.logoutUrl ?: "https://auth.ala.org.au/cas/logout"
 
     static namespace = "auth"
     //static encodeAsForTags = [tagName: 'raw']
@@ -47,6 +55,78 @@ class AuthTagLib {
      */
     def ifNotGranted = { attrs, body ->
         if (securityPrimitives.isNotGranted(rolesStringToList(attrs))) out << body()
+    }
+
+    /**
+     * Generate the login/logout link (taken from ala-web-theme plugin)
+     *
+     * @attr cssClass - CSS class to add to a tag
+     *
+     * plus
+     * @attr logoutUrl the local url that should invalidate the session and redirect to the auth
+     *  logout url - defaults to {CH.config.grails.serverURL}/session/logout
+     * @attr loginReturnToUrl where to go after logging in - defaults to current page
+     * @attr logoutReturnToUrl where to go after logging out - defaults to current page
+     * @attr loginReturnUrl where to go after login - defaults to current page
+     * @attr casLoginUrl - defaults to {CH.config.security.cas.loginUrl}
+     * @attr casLogoutUrl - defaults to {CH.config.security.cas.logoutUrl}
+     * @attr ignoreCookie - if true the helper cookie will not be used to determine login - defaults to false
+     */
+    def loginLogout = { attrs ->
+        out << buildLoginoutLink(attrs)
+    }
+
+    /**
+     * Builds the login or logout link based on current login status.
+     * @param attrs any specified params to override defaults
+     * @return
+     */
+    String buildLoginoutLink(attrs) {
+        def requestUri = removeContext(grailServerURL) + request.forwardURI
+        def logoutUrl = attrs.logoutUrl ?: grailServerURL + "/session/logout"
+        def logoutReturnToUrl = attrs.logoutReturnToUrl ?: requestUri
+        def casLogoutUrl = attrs.casLogoutUrl ?: casLogoutUrl
+
+        // TODO should this be attrs.logoutReturnToUrl?
+        if (!attrs.loginReturnToUrl && request.queryString) {
+            logoutReturnToUrl += "?" + URLEncoder.encode(request.queryString, "UTF-8")
+        }
+
+        if ((attrs.ignoreCookie != "true" &&
+                AuthenticationCookieUtils.cookieExists(request, AuthenticationCookieUtils.ALA_AUTH_COOKIE)) ||
+                request.userPrincipal) {
+            return "<a href='${logoutUrl}" +
+                    "?casUrl=${casLogoutUrl}" +
+                    "&appUrl=${logoutReturnToUrl}' " +
+                    "class='${attrs.cssClass}'>Logout</a>"
+        } else {
+            // currently logged out
+            return "<a href='${buildLoginLink(attrs)}' class='${attrs.cssClass}'><span>Log in</span></a>"
+        }
+    }
+
+    /**
+     * Build the login link
+     * @param attrs any specified params to override defaults
+     * @return The login url
+     */
+    String buildLoginLink(attrs) {
+        def casLoginUrl = attrs.casLoginUrl ?: casLoginUrl
+        def loginReturnToUrl = attrs.loginReturnToUrl ?: (removeContext(grailServerURL) + request.forwardURI + (request.queryString ? "?" + URLEncoder.encode(request.queryString, "UTF-8") : ""))
+        String loginUrl = "${casLoginUrl}?service=${loginReturnToUrl}"
+        return loginUrl
+    }
+
+    /**
+     * Remove the context path and params from the url.
+     * @param urlString
+     * @return
+     */
+    private String removeContext(urlString) {
+        def url = urlString.toURL()
+        def protocol = url.protocol != -1 ? url.protocol + "://" : ""
+        def port = url.port != -1 ? ":" + url.port : ""
+        return protocol + url.host + port
     }
 
     private def rolesStringToList(attrs) {
