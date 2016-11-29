d6b877f10b897bf01e98b6250dd567168726b1ec
Simon Bear <simon.bear@csiro.au>
2016-11-29 17:12:19 +1100

    Increment version, take 2

--- ala-ws-plugin/AlaWsPluginGrailsPlugin.groovy
+++ ala-ws-plugin/AlaWsPluginGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaWsPluginGrailsPlugin {
     // the plugin version
-    def version = "1.5"
+    def version = "1.5.1-SNAPSHOT"
 
 
     // the version or versions of Grails the plugin is designed for
