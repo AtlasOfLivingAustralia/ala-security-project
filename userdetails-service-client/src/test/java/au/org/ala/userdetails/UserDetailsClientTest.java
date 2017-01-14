d14ebcbc3553a5ecab8ded99a4910847a39de5d5
Simon Bear <simon.bear@csiro.au>
2017-01-14 16:05:25 +1100

    Switch AssertJ to v2 for JDK7 compatibility

--- userdetails-service-client/src/test/java/au/org/ala/userdetails/UserDetailsClientTest.java
+++ userdetails-service-client/src/test/java/au/org/ala/userdetails/UserDetailsClientTest.java
@@ -4,7 +4,6 @@ import au.org.ala.web.UserDetails;
 import com.google.common.collect.ImmutableMap;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
