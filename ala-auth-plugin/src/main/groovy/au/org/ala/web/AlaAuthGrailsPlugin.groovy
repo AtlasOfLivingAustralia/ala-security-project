154934a9b87e20d99e9d95697d94e87c3975b0a8
Simon Bear <simon.bear@csiro.au>
2017-01-18 17:34:23 +1100

    Add AlaAuthGrailsPlugin.groovy changes

--- ala-auth-plugin/src/main/groovy/au/org/ala/web/AlaAuthGrailsPlugin.groovy
+++ ala-auth-plugin/src/main/groovy/au/org/ala/web/AlaAuthGrailsPlugin.groovy
@@ -1,23 +1,34 @@
-import au.org.ala.web.SecurityPrimitives
-import grails.util.Holders
+package au.org.ala.web
 
-class AlaAuthGrailsPlugin {
-    // the plugin version
-    def version = "1.3.4"
+import au.org.ala.cas.client.AlaHttpServletRequestWrapperFilter
+import au.org.ala.cas.client.UriFilter
+import au.org.ala.web.config.PluginConfig
+import grails.plugins.*
+import org.jasig.cas.client.authentication.AuthenticationFilter
+import org.jasig.cas.client.session.SingleSignOutFilter
+import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+
+class AlaAuthPluginGrailsPlugin extends Plugin {
+
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
+    def grailsVersion = "3.2.4 > *"
     // resources that are excluded from plugin packaging
-    def pluginExcludes = ["grails-app/views/error.gsp"]
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
 
     def title = "Ala Auth Plugin" // Headline display name of the plugin
     def author = "Nick dos Remedios"
     def authorEmail = "nick.dosremedios@csiro.au"
-    def description = "This plugin provides auth services for ALA."
+    def description = '''\
+This plugin provides auth services for ALA.
+'''
 
     // URL to the plugin's documentation
     def documentation = "https://github.com/AtlasOfLivingAustralia/ala-auth-plugin"
+
+    // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "MPL2"
@@ -26,7 +37,7 @@ class AlaAuthGrailsPlugin {
     def organization = [ name: "Atlas of Living Australia", url: "http://www.ala.org.au/" ]
 
     // Any additional developers beyond the author specified above.
-    def developers = [ [ name: "Nick dos Remedios", email: "nick.dosremedios@csiro.au" ], [ name: "Dave Martin", email: "david.martin@csiro.au" ]]
+    def developers = [ [ name: "Simon Bear", email: "simon.bear@csiro.au" ], [ name: "Chris Godwin", email: "chris.godwin.ala@gmail.com" ], [ name: "Peter Ansell", email: "p_ansell@yahoo.com" ], [ name: "Dave Martin", email: "david.martin@csiro.au" ]]
 
     // Location of the plugin's issue tracker.
     def issueManagement = [ system: "github", url: "https://github.com/AtlasOfLivingAustralia/ala-auth-plugin/issues" ]
@@ -34,195 +45,69 @@ class AlaAuthGrailsPlugin {
     // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/AtlasOfLivingAustralia/ala-auth-plugin" ]
 
-    // Note: ONLY evaluated at compile time (not run time)
-    def doWithWebDescriptor = { xml ->
-        def mappingElement = xml.'context-param'
-        def lastMapping = mappingElement[mappingElement.size()-1]
-        String defaultConfig = Holders.config.default_config
+    Closure doWithSpring() { {->
+            casContextParamInitializer(CasContextParamInitializer)
 
-        if (Holders.config.runWithNoExternalConfig == true) {
-            // No external config file set, use security.cas.* settings embedded un web.xml instead
-            if (!Holders.config.security.cas || Holders.config.security.cas.size() == 0) {
-                println "No security.cas.* settings found (or external conf found) - setting bypass = true"
-                Holders.config.security.cas.bypass = true
+            casSSOFilter(FilterRegistrationBean) {
+                name = 'Cas Single Sign Out Filter'
+                filter = bean(SingleSignOutFilter)
+                order = 1
+                urlPatterns = ['/*']
+            }
+            casAuthFilter(FilterRegistrationBean) {
+                name = 'CAS Authentication Filter'
+                filter = bean(UriFilter)
+                order = 2
+                urlPatterns = ['/*']
+                initParameters = [
+                        'filterClass': AuthenticationFilter.name,
+                        'casServerLoginUrl': grailsApplication.config.security.cas.loginUrl,
+                        'gateway': 'false',
+                        'disableCAS': grailsApplication.config.security.cas.bypass.toString()
+                ]
+            }
+            casValidationFilter(FilterRegistrationBean) {
+                name = 'CAS Validation Filter'
+                filter = bean(UriFilter)
+                order = 3
+                urlPatterns = ['/*']
+                initParameters = [
+                        'filterClass': Cas20ProxyReceivingTicketValidationFilter.name,
+                        'disableCAS': grailsApplication.config.security.cas.bypass.toString(),
+                        'casServerUrlPrefix': grailsApplication.config.security.cas.casServerUrlPrefix
+                ]
+            }
+            casHttpServletRequestWrapperFilter(FilterRegistrationBean) {
+                name = 'CAS HttpServletRequest Wrapper Filter'
+                filter = bean(UriFilter)
+                order = 4
+                urlPatterns = ['/*']
+                initParameters = [
+                        'filterClass': AlaHttpServletRequestWrapperFilter.name,
+                        'disableCAS': grailsApplication.config.security.cas.bypass.toString(),
+                ]
             }
 
-            lastMapping + {
-                'context-param' {
-                    'param-name' ('serverName')
-                    'param-value' (Holders.config.security.cas.appServerName)
-                }
-                'context-param' {
-                    'param-name' ('casServerName')
-                    'param-value' (Holders.config.security.cas.casServerName)
-                }
-                'context-param' {
-                    'param-name' ('uriFilterPattern')
-                    'param-value' (Holders.config.security.cas.uriFilterPattern)
-                }
-                'context-param' {
-                    'param-name' ('uriExclusionFilterPattern')
-                    'param-value' (Holders.config.security.cas.uriExclusionFilterPattern)
-                }
-                'context-param' {
-                    'param-name' ('authenticateOnlyIfLoggedInFilterPattern')
-                    'param-value' (Holders.config.security.cas.authenticateOnlyIfLoggedInPattern)
-                }
-            }
+            alaAuthPluginConfiguration(PluginConfig)
 
-            if (Holders.config.security.cas.contextPath) {
-                lastMapping + {
-                    'context-param' {
-                        'param-name' ('contextPath')
-                        'param-value' (Holders.config.security.cas.contextPath)
-                    }
-                }
+            securityPrimitives(SecurityPrimitives) { beanDefinition ->
+                beanDefinition.constructorArgs = [ref('authService'), ref('grailsApplication')]
             }
-        } else {
-            // Use `configPropFile` external config which is read by ala-cas-client-2.0-SNAPSHOT.jar
-            lastMapping + {
-                'env-entry' {
-                    'env-entry-name' ('configPropFile')
-                    'env-entry-value' (defaultConfig)
-                    'env-entry-type' ('java.lang.String')
-                }
-            }
-        }
-
-        mappingElement = xml.'filter'
-        lastMapping = mappingElement[mappingElement.size()-1]
-        lastMapping + {
-            'filter' {
-                'filter-name' ('CAS Single Sign Out Filter')
-                'filter-class' ('org.jasig.cas.client.session.SingleSignOutFilter')
-            }
-            'filter' {
-                'filter-name' ('CAS Authentication Filter')
-                'filter-class' ('au.org.ala.cas.client.UriFilter')
-                'init-param' {
-                    'param-name' ('filterClass')
-                    'param-value' ('org.jasig.cas.client.authentication.AuthenticationFilter')
-                }
-                'init-param' {
-                    'param-name' ('casServerLoginUrl')
-                    'param-value' (Holders.config.security.cas.loginUrl)
-                }
-                'init-param' {
-                    'param-name' ('gateway')
-                    'param-value' ('false')
-                }
-                'init-param' {
-                    'param-name' ('disableCAS')
-                    'param-value' (Holders.config.security.cas.bypass == true ? 'true' : 'false')
-                }
-            }
-            'filter' {
-                'filter-name' ('CAS Validation Filter')
-                'filter-class' ('au.org.ala.cas.client.UriFilter')
-                'init-param' {
-                    'param-name' ('filterClass')
-                    'param-value' ('org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter')
-                }
-                'init-param' {
-                    'param-name' ('disableCAS')
-                    'param-value' (Holders.config.security.cas.bypass == true ? 'true' : 'false')
-                }
-                'init-param' {
-                    'param-name' ('casServerUrlPrefix')
-                    'param-value' (Holders.config.security.cas.casServerUrlPrefix)
-                }
-            }
-            'filter' {
-                'filter-name' ('CAS HttpServletRequest Wrapper Filter')
-                'filter-class' ('au.org.ala.cas.client.UriFilter')
-                'init-param' {
-                    'param-name' ('filterClass')
-                    'param-value' ('au.org.ala.cas.client.AlaHttpServletRequestWrapperFilter')
-                }
-                'init-param' {
-                    'param-name' ('disableCAS')
-                    'param-value' (Holders.config.security.cas.bypass == true ? 'true' : 'false')
-                }
-            }
-            'filter-mapping' {
-                'filter-name' ('CAS Single Sign Out Filter')
-                'url-pattern' ('/*')
-            }
-            'filter-mapping' {
-                'filter-name' ('CAS Authentication Filter')
-                'url-pattern' ('/*')
-            }
-            'filter-mapping' {
-                'filter-name' ('CAS Validation Filter')
-                'url-pattern' ('/*')
-            }
-            'filter-mapping' {
-                'filter-name' ('CAS HttpServletRequest Wrapper Filter')
-                'url-pattern' ('/*')
-            }
-        }
-
-        if (Holders.config.security.cas.debugWebXml) {
-            println "web.xml = ${mappingElement}"
         }
     }
 
-    def doWithSpring = {
-        //System.println("Merging conf...")
-        //mergeConfig(application)
-        def config = application.config
-
-        if (!config.userDetails.url) {
-            config.userDetails.url = "https://auth.ala.org.au/userdetails/userDetails/"
-        }
-
-        if (!config.userDetails.path) {
-            config.userDetails.path = "getUserListFull"
-        }
-
-        if (!config.userDetailsById.path) {
-            config.userDetailsById.path = "getUserDetails"
-        }
-
-        if (!config.userDetailsById.bulkPath) {
-            config.userDetailsById.bulkPath = 'getUserDetailsFromIdList'
-        }
-
-        if (!config.grails.cache.config) {
-            config.grails.cache.config = {
-                defaults {
-                    eternal false
-                    overflowToDisk false
-                    maxElementsInMemory 20000
-                    timeToLiveSeconds 3600
-                }
-                cache {
-                    name 'userListCache'
-                }
-                cache {
-                    name 'userMapCache'
-                }
-
-            }
-        }
-
-        securityPrimitives(SecurityPrimitives) { beanDefinition ->
-            beanDefinition.constructorArgs = [ref('authService'), ref('grailsApplication')]
-        }
+    void doWithDynamicMethods() {
     }
 
-    def doWithDynamicMethods = { ctx ->
+    void doWithApplicationContext() {
     }
 
-    def doWithApplicationContext = { applicationContext ->
+    void onChange(Map<String, Object> event) {
     }
 
-    def onChange = { event ->
+    void onConfigChange(Map<String, Object> event) {
     }
 
-    def onConfigChange = { event ->
-    }
-
-    def onShutdown = { event ->
+    void onShutdown(Map<String, Object> event) {
     }
 }
