5d012912a0b37a2efb70836e36dd74c82fece8a4
Simon Bear <simon.bear@csiro.au>
2017-05-09 03:04:10 +1000

    Use release versions of userdetails client and ALA CAS client libraries

--- ala-auth-plugin/grails-app/conf/BuildConfig.groovy
+++ ala-auth-plugin/grails-app/conf/BuildConfig.groovy
@@ -24,8 +24,8 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.18'
-        compile ('au.org.ala:ala-cas-client:2.3-SNAPSHOT')
-        compile ('au.org.ala:userdetails-service-client:1.0.0-SNAPSHOT')
+        compile ('au.org.ala:ala-cas-client:2.3')
+        compile ('au.org.ala:userdetails-service-client:1.0.0')
 
         test ('com.squareup.retrofit2:retrofit-mock:2.2.0')
     }
