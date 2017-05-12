6f767e3ed958e5729d02b4826170692d360ce45b
Simon Bear <simon.bear@csiro.au>
2017-05-12 16:12:33 +1000

    Create new snapshot version using updated ala-auth library

--- ala-ws-plugin/AlaWsPluginGrailsPlugin.groovy
+++ ala-ws-plugin/AlaWsPluginGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class AlaWsPluginGrailsPlugin {
     // the plugin version
-    def version = "1.6-SNAPSHOT"
+    def version = "1.6.1-SNAPSHOT"
 
 
     // the version or versions of Grails the plugin is designed for
