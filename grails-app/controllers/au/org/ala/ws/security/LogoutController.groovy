799f9a5c89e8b30e9c7e4f78a0117ad7e3170529
Dave Martin <djtfmartin@gmail.com>
2021-11-12 09:48:10 +0000

    AlaOAuth2UserService and grails 4

---
+++ grails-app/controllers/au/org/ala/ws/security/LogoutController.groovy
@@ -0,0 +1,29 @@
+package au.org.ala.ws.security
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler
+
+class LogoutController {
+
+    /**
+     * Do logouts through this app so we can invalidate the session.
+     *
+     * @param casUrl the url for logging out of cas
+     * @param appUrl the url to redirect back to after the logout
+     */
+    def logout() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        authentication.setAuthenticated(false);
+        new SecurityContextLogoutHandler().logout(request,response,authentication);
+
+        SecurityContextHolder.clearContext();
+        request.logout()
+        session.invalidate()
+
+        def appUrl = URLEncoder.encode(params.appUrl ?: g.createLink(uri: '/'), "UTF-8")
+        def casUrl = grailsApplication.config.security.logoutUrl ?: grailsApplication.config.security.cas.logoutUrl
+        redirect(url:"${casUrl}?url=${appUrl}")
+    }
+}
---
+++ src/test/groovy/au/org/ala/ws/security/LogoutControllerSpec.groovy
@@ -0,0 +1,18 @@
+package au.org.ala.ws.security
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class LogoutControllerSpec extends Specification implements ControllerUnitTest<LogoutController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
