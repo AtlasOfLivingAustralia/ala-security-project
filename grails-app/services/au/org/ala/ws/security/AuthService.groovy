4124e842ab6819ba622a35ce305de1bbb077a031
Dave Martin <djtfmartin@gmail.com>
2021-11-13 11:52:45 +0000

    rationalise configuration, AuthService port, attributes included in JWT auth

---
+++ grails-app/services/au/org/ala/ws/security/AuthService.groovy
@@ -0,0 +1,48 @@
+package au.org.ala.ws.security
+
+import org.springframework.web.context.request.RequestContextHolder
+
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * TODO - Port these methods to OAuth - remove cookie logic
+ */
+class AuthService {
+
+    def grailsApplication
+
+    def serviceMethod() {}
+
+    def getEmail() {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        request.getUserPrincipal()?.getName()
+    }
+
+    def getUserId() {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        request.getUserPrincipal()?.principal?.attributes?.userid
+    }
+
+    def getDisplayName() {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        request.getUserPrincipal()?.principal?.attributes?.firstname
+    }
+
+    def getFirstName() {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        request.getUserPrincipal()?.principal?.attributes?.firstname
+    }
+
+    def getLastName() {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        request.getUserPrincipal()?.principal?.attributes?.lastname
+    }
+
+    boolean userInRole(role) {
+        def request = RequestContextHolder.currentRequestAttributes().getRequest() as HttpServletRequest
+        def inRole = request.isUserInRole(role)
+        def bypass = grailsApplication.config.security.cas.bypass
+        log.debug("userInRole(${role}) - ${inRole} (bypassing CAS - ${bypass})")
+        return bypass.toString().toBoolean() || inRole
+    }
+}
