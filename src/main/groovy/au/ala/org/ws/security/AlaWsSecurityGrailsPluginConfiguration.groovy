3ad2522933fdb52a9470f6a12483f06a89483a73
Simon Bear <simon.bear@csiro.au>
2022-07-14 20:12:12 +1000

    Add missing import

--- src/main/groovy/au/ala/org/ws/security/AlaWsSecurityGrailsPluginConfiguration.groovy
+++ src/main/groovy/au/ala/org/ws/security/AlaWsSecurityGrailsPluginConfiguration.groovy
@@ -17,6 +17,7 @@ import org.pac4j.core.context.WebContextFactory
 import org.pac4j.core.context.session.SessionStore
 import org.pac4j.core.engine.DefaultSecurityLogic
 import org.pac4j.http.client.direct.DirectBearerAuthClient
+import org.pac4j.jee.context.JEEContextFactory
 import org.pac4j.jee.context.session.JEESessionStore
 import org.pac4j.jee.filter.SecurityFilter
 import org.springframework.beans.factory.annotation.Autowired
