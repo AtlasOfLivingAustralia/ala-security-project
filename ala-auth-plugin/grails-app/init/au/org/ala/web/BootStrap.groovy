9df736b4580c41342c8e001a92aeb1ca60b55c05
Simon Bear <simon.bear@csiro.au>
2022-08-05 10:29:40 +1000

    Use core auth cookie name property for AuthenticationCookieUtils system property override

--- ala-auth-plugin/grails-app/init/au/org/ala/web/BootStrap.groovy
+++ ala-auth-plugin/grails-app/init/au/org/ala/web/BootStrap.groovy
@@ -5,9 +5,13 @@ class BootStrap {
     def grailsApplication
 
     def init = { servletContext ->
-        def cookieName = grailsApplication.config.getProperty('security.cas.authCookieName')
+        // hack to load the auth cookie name into a system property from the grails config properties before the
+        // AuthenticationCookieUtils static initializer runs
+        def config = grailsApplication.config
+
+        def cookieName = config.getProperty('security.core.auth-cookie-name') ?: config.getProperty('security.core.authCookieName') ?: config.getProperty('security.cas.authCookieName')
         if (cookieName) {
-            System.setProperty('ala.auth.cookie.name',  cookieName)
+            System.setProperty('ala.auth.cookie.name', cookieName)
         }
     }
     def destroy = {
