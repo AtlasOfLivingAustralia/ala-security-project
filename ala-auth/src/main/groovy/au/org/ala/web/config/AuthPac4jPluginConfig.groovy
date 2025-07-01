90ecdc5d8a7a878b9810f43e11205e9aa3283e07
Simon Bear <simon.bear@csiro.au>
2025-07-01 17:19:48 +1000

    Remove early init of OidcClient objects

--- ala-auth/src/main/groovy/au/org/ala/web/config/AuthPac4jPluginConfig.groovy
+++ ala-auth/src/main/groovy/au/org/ala/web/config/AuthPac4jPluginConfig.groovy
@@ -162,7 +162,7 @@ class AuthPac4jPluginConfig {
         def client = createOidcClientFromConfig(oidcConfiguration, authCookieGenerator)
         client.setName(DEFAULT_CLIENT)
         client.setCredentialsExtractor(new CognitoOidcExtractor(oidcConfiguration, client))
-        client.init()
+//        client.init()
         return client
     }
 
@@ -175,7 +175,7 @@ class AuthPac4jPluginConfig {
         config.init()
         def client = createOidcClientFromConfig(config, authCookieGenerator)
         client.setName(PROMPT_NONE_CLIENT)
-        client.init()
+//        client.init()
         return client
     }
 
