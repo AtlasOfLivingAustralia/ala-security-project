e19f5b2454c1ffb00d7baba83a77742948371019
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-10 15:07:29 +1000

    Another fix for client failling to extract principal from request
    This works, as confirmed by running `ala-ws-spring-security:publishToMavenLocal` and testing in species-lists.

--- ala-ws-security/src/main/java/au/org/ala/ws/security/client/AlaAuthClient.java
+++ ala-ws-security/src/main/java/au/org/ala/ws/security/client/AlaAuthClient.java
@@ -43,7 +43,14 @@ public class AlaAuthClient extends BaseClient {
 
                 final Optional<Credentials> optCredentials = authClient.getCredentials(ctx);
                 if (optCredentials.isPresent()) {
-                    return authClient.validateCredentials(ctx, optCredentials.get());
+                    final Optional<Credentials> validatedCredentials =
+                            authClient.validateCredentials(ctx, optCredentials.get());
+                    if (validatedCredentials.isPresent()) {
+                        final Optional<UserProfile> profile =
+                                authClient.getUserProfile(ctx, validatedCredentials.get());
+                        profile.ifPresent(validatedCredentials.get()::setUserProfile);
+                        return validatedCredentials;
+                    }
                 }
             }
         } catch (CredentialsException e) {
