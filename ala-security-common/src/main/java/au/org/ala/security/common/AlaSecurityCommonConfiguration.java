936aabe08bd87e0342bbb4d5ddcabf404435b88b
Nick dos Remedios <nick.dosremedios@csiro.au>
2026-09-14 12:19:02 +1000

    Revert unecessary @ConditionalOnExpression change
    Gemini reports "string equals" check is not required and suggests default value to avoid NPE.

--- ala-security-common/src/main/java/au/org/ala/security/common/AlaSecurityCommonConfiguration.java
+++ ala-security-common/src/main/java/au/org/ala/security/common/AlaSecurityCommonConfiguration.java
@@ -34,7 +34,7 @@ public class AlaSecurityCommonConfiguration {
 
     @Autowired
     OidcClientProperties oidcClientProperties;
-    @ConditionalOnExpression("'${security.oidc.enabled}' == 'true' or '${security.jwt.enabled}' == 'true'")
+    @ConditionalOnExpression("${security.oidc.enabled:false} || ${security.jwt.enabled:false}")
     @Bean
     public Retry oidcRetry() {
 
@@ -50,7 +50,7 @@ public class AlaSecurityCommonConfiguration {
         return Retry.of("oidc", config);
     }
 
-    @ConditionalOnExpression("'${security.oidc.enabled}' == 'true' or '${security.jwt.enabled}' == 'true'")
+    @ConditionalOnExpression("${security.oidc.enabled:false} || ${security.jwt.enabled:false}")
     @Bean
     public ResourceRetriever oidcResourceRetriever(@Qualifier("oidcRetry") Retry oidcRetry) {
         DefaultResourceRetriever resourceRetriever = new DefaultResourceRetriever(oidcClientProperties.getConnectTimeout(), oidcClientProperties.getReadTimeout());
